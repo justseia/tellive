1280511a1b1f04c251503f36864d03967f2df8b9
justseia <seiitmurat.bekmuratuly@gmail.com>
2025-06-03 20:01:03 +0500

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,8 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
+
+document.addEventListener('gesturestart', function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 1;
+});
